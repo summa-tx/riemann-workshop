97b90d278e8f14cdb9be0c039c673bd4f2b168bc
James Prestwich <james@prestwi.ch>
2020-02-07 01:09:52 -0800

    Comments and docstrings for crypto and inspect

--- workshop/crypto.py
+++ workshop/crypto.py
@@ -1,10 +1,52 @@
+import os
+
+# ecdsa is a tight pure python library maintained by Brian Warner
 from ecdsa import SigningKey, SECP256k1
 from ecdsa.util import sigencode_der_canonize
 
 from typing import cast
 
 
+def fake_privkey() -> bytes:
+    '''
+    Generate a dummy private key.
+    Useful for tests that need to be deterministic.
+
+    # WARNING: Do not use this in production.
+    '''
+    return b'\x01' * 32
+
+
+def new_privkey() -> bytes:
+    '''
+    Generate a new private key from os entropy
+    Check out riemann-keys for a full BIP32/39/44 ~experience~
+    https://github.com/summa-tx/riemann-keys
+
+    # WARNING: This is not best practices.
+    '''
+    return os.urandom(32)
+
+
 def sign_digest(digest: bytes, privkey: bytes) -> bytes:
+    '''
+    Sign a digest with a private key
+
+    Bitcoin signatures must be DER encoded, so we pass in an encoder. The
+    encoder also 'canonizes' the s-value of the signature. Bitcoin enforces
+    low-s ECDSA signatures.
+
+    Args:
+        digest: the 32-byte digest to sign
+        privkey: the 32-byte key to sign with
+
+    Returns:
+        A DER-encoded signature
+    '''
+    if len(digest) != 32:
+        raise ValueError(f'digest must be 32 bytes. Got {len(digest)}')
+    if len(privkey) != 32:
+        raise ValueError(f'privkey must be 32 bytes. Got {len(privkey)}')
     signing_key = SigningKey.from_string(privkey, SECP256k1)
     sig = signing_key.sign_digest_deterministic(
         digest=digest,
@@ -13,21 +55,38 @@ def sign_digest(digest: bytes, privkey: bytes) -> bytes:
     return cast(bytes, sig)
 
 
-def priv_to_pub(privkey: bytes) -> bytes:
-    pubkey = SigningKey.from_string(privkey, SECP256k1).verifying_key
-    return compress_pubkey(pubkey.to_string())
-
-
-def compress_pubkey(pubkey: bytes) -> bytes:
+def _compress_raw_pubkey(pubkey: bytes) -> bytes:
+    '''
+    Return the compressed key corresponding to a raw key
+    '''
+    if len(pubkey) != 64:
+        raise ValueError(f'pubkey must be 64 bytes. Got {len(pubkey)}')
     parity = (pubkey[-1] & 1) + 2
     compressed = bytes([parity]) + pubkey[:32]
     return compressed
 
 
+def priv_to_pub(privkey: bytes) -> bytes:
+    '''
+    Return the compressed public key corresponding to a private key.
+    SegWit txns enforce the use of compressed pubkeys. In legacy txns, you
+    may still use uncompressed keys. 
+
+    Args:
+        privkey: a 32-byte private key
+    Returns:
+        A 33-byte compressed pubkey, suitable for use in Bitcoin txns
+    '''
+    if len(privkey) != 32:
+        raise ValueError(f'privkey must be 32 bytes. Got {len(privkey)}')
+
+    pubkey = SigningKey.from_string(privkey, SECP256k1).verifying_key
+    return _compress_raw_pubkey(pubkey.to_string())
+
+
 def pow_mod(x: int, y: int, z: int) -> int:
     '''
-    int, int, int (or float)
-    returns (x^y)mod z
+    (x ** y) % z
     '''
     number = 1
     while y:
@@ -40,7 +99,7 @@ def pow_mod(x: int, y: int, z: int) -> int:
 
 def uncompress_pubkey(pubkey: bytes) -> bytes:
     '''
-    takes a compressed pubkey, returns the uncompressed pubkey (64 bytes)
+    Returns the raw pubkey (64 bytes) corresponding to a compressed pubkey
     '''
     p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
     parity = pubkey[0] - 2
--- workshop/inspect.py
+++ workshop/inspect.py
@@ -3,25 +3,40 @@ from riemann import utils as rutils
 
 from typing import List
 
+
+# # One sample block included in the repo
+# # You can get more from blockchain.info
+# https://blockchain.info/rawblock/000000000000000000059d8621464f112eb538abb24f60771853aa890e61fa20?format=hex
+
 with open('data/000000000000000000059d8621464f112eb538abb24f60771853aa890e61fa20') as file:  # noqa: E501
     block_bytes = bytes.fromhex(file.read())
 
 
 def as_btc(sats: float) -> str:
+    '''
+    Format a number as an amount of BTC for display to user
+
+    # WARNING: This is not best practices. Float math will drop sats.
+               Use integers wherever math is important
+    '''
     return f'{sats / 100_000_000:.8f} BTC'
 
-
+# The first 80 bytes of a block is the header
 header = block_bytes[:80]
 
-# a serialized block specifies how many txns are in it
+# Then, the block specifies how many txns are in it
 num_txns = tx.VarInt.from_bytes(block_bytes[80:])
 num_txns_len = len(num_txns)
 
+# The rest of the block is the txns in a blob
 txn_bytes = block_bytes[80 + num_txns_len:]
 
 offset = 0
 txns: List[tx.Tx] = []
 
+# Loop through the txn_bytes, parsing txns off the front
+# Riemann parsers all silently handle extra bytes.
+# Each time we call from_bytes it returns a tx from the front of the bytearray
 for i in range(num_txns.number):
     try:
         txns.append(tx.Tx.from_bytes(txn_bytes[offset:]))
@@ -33,10 +48,14 @@ for i in range(num_txns.number):
         print(txn_bytes[offset:].hex())
         break
 
-
-vals_out: List[int] = [sum(rutils.le2i(tx_out.value) for tx_out in t.tx_outs) for t in txns]  # noqa: E501
+# list the total output value of each tx
+vals_out: List[int] = [
+    sum(rutils.le2i(tx_out.value) for tx_out in t.tx_outs)
+    for t in txns
+]
 num_outputs = sum(len(t.tx_outs) for t in txns)
 
+# Calculate and output some basic stats
 print()
 print(f'block produced {num_outputs} new TXOs')
 print(f'total output value of block is {as_btc(sum(vals_out))}')
